ea8af3a4253db0f25f6a49fd68611a81f044879c
quangBig <ledai16102004@gmail.com>
2025-10-03 15:16:03 +0700

    update file .env

--- backend/src/database/database.providers.ts
+++ backend/src/database/database.providers.ts
@@ -1,17 +1,20 @@
 import { createPool } from "mysql2/promise";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export const DatabaseProviders = {
     provide: "DATABASE_CONNECTION",
     useFactory: async () => {
         try {
             const pool = createPool({
-                host: '52.175.37.189',   // Đúng IP server
-                port: 5001,              // Đúng port MySQL
-                user: 'root',
-                password: '123qwe!@#',
-                database: 'HospitalERP108',
+                host: process.env.DB_HOST,
+                port: Number(process.env.DB_PORT),
+                user: process.env.DB_USER,
+                password: process.env.DB_PASS,
+                database: process.env.DB_NAME,
                 waitForConnections: true,
-                connectionLimit: 10,     // Nên giới hạn pool
+                connectionLimit: 10,
                 queueLimit: 0,
             });
 
